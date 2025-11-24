951a762f9ecee8635cc80075f7f61912e42a6a4b
Clayton Smith <clayton.eric.smith@gmail.com>
2025-11-24 02:33:49 +0000

    Adds Hal program

---
+++ hal-9000.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#A script to demo user input and variable use
+
+echo "Hello, I'm HAL 9000. What is your name?"
+
+read USERNAME
+
+echo "I'm sorry $USERNAME. I'm afaid I can't open the pod bay doors"
+
+ 
